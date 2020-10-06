1feabaf51b19854b559e77a8d1d506e4be0abdc7
u3556571 <47150468+u3556571@users.noreply.github.com>
2020-10-06 17:10:51 +0800

    Update monster UI 2

--- app/src/main/java/hku/hkucs/mcrpg/MainActivity.java
+++ app/src/main/java/hku/hkucs/mcrpg/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity { //Leon: this script will a
     TextView ability2_cd;
     TextView ability3_cd;
     TextView ability4_cd;
+    ImageView effect_fire1;
+    ImageView effect_fire2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +32,14 @@ public class MainActivity extends AppCompatActivity { //Leon: this script will a
         ability2_cd = findViewById(R.id.textView_ability2_cd);
         ability3_cd = findViewById(R.id.textView_ability3_cd);
         ability4_cd = findViewById(R.id.textView_ability4_cd);
+        effect_fire1 = findViewById(R.id.imageView_effect_fire1);
+        effect_fire2 = findViewById(R.id.imageView_effect_fire2);
 
         // button for testing
         Button button_attack = findViewById(R.id.button);
         Button button_reset = findViewById(R.id.button2);
+        Button button_onFire = findViewById(R.id.button3);
+        Button button_offFire = findViewById(R.id.button4);
 
         button_attack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +58,22 @@ public class MainActivity extends AppCompatActivity { //Leon: this script will a
             }
         });
 
+        button_onFire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                monster.setOnFire();
+                UpdateUI();
+            }
+        });
+
+        button_offFire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                monster.setOffFire();
+                UpdateUI();
+            }
+        });
+
         UpdateUI();
     }
 
@@ -60,5 +83,13 @@ public class MainActivity extends AppCompatActivity { //Leon: this script will a
         ability2_cd.setText("CD " + monster.getAbility_2_cd());
         ability3_cd.setText("CD " + monster.getAbility_3_cd());
         ability4_cd.setText("CD " + monster.getAbility_4_cd());
+        if (monster.getFireStatus()) {
+            effect_fire1.setVisibility(View.VISIBLE);
+            effect_fire2.setVisibility(View.VISIBLE);
+        }
+        else {
+            effect_fire1.setVisibility(View.INVISIBLE);
+            effect_fire2.setVisibility(View.INVISIBLE);
+        }
     }
 }
