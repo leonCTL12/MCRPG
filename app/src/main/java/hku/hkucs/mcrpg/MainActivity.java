44caf43fb43db9cbc3da52fa9c589b24e177cebf
LeonCTL <leonchantinglok@gmail.com>
2020-10-10 11:33:22 +0800

    Added player UI

--- app/src/main/java/hku/hkucs/mcrpg/MainActivity.java
+++ app/src/main/java/hku/hkucs/mcrpg/MainActivity.java
@@ -3,15 +3,17 @@ package hku.hkucs.mcrpg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity { //Leon: this script will act as something like game manager in Unity
 
-    Player player;
+    //Monster
     monster monster;
     ProgressBar monsterHealthBar;
     TextView ability1_cd;
@@ -21,12 +23,39 @@ public class MainActivity extends AppCompatActivity { //Leon: this script will a
     ImageView effect_fire1;
     ImageView effect_fire2;
 
+    //Player
+    Player player;
+    ProgressBar playerHealthBar;
+    Button optionA;
+    Button optionB;
+    Button optionC;
+    Button optionD;
+    ImageButton skill1;
+    ImageButton skill2;
+    ImageButton skill3;
+    ImageButton skill4;
+    TextView skill1CD;
+    TextView skill2CD;
+    TextView skill3CD;
+    TextView skill4CD;
+
+    //Timer
+    TextView timer;
+    Thread timerThread;
+    Thread updateThread;
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // initialize
+        //Monster
         monster = new monster();
         monsterHealthBar = findViewById(R.id.progressBar);
         ability1_cd = findViewById(R.id.textView_ability1_cd);
@@ -36,8 +65,74 @@ public class MainActivity extends AppCompatActivity { //Leon: this script will a
         effect_fire1 = findViewById(R.id.imageView_effect_fire1);
         effect_fire2 = findViewById(R.id.imageView_effect_fire2);
 
+        //Player
+        player = new Player();
+        playerHealthBar = findViewById(R.id.PlayerHP);
+        optionA = findViewById(R.id.OptionA);
+        optionB = findViewById(R.id.OptionB);
+        optionC = findViewById(R.id.OptionC);
+        optionD = findViewById(R.id.OptionD);
+        skill1 = findViewById(R.id.Skill1);
+        skill2 = findViewById(R.id.Skill2);
+        skill3 = findViewById(R.id.Skill3);
+        skill4 = findViewById(R.id.Skill4);
+        skill1CD = findViewById(R.id.Skill1CD);
+        skill2CD = findViewById(R.id.Skill2CD);
+        skill3CD = findViewById(R.id.Skill3CD);
+        skill4CD = findViewById(R.id.Skill4CD);
+
+        //Timer
+        player.startTimer(); //testing timer
+        timer = findViewById(R.id.Timer);
+        timerThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    while (!timerThread.isInterrupted()) {
+                        Thread.sleep(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // update TextView here!
+                                timer.setText(String.valueOf(player.getTimeLeft()));
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+        timerThread.start();
+
+        //general
+        //Leon: Can we implement sth like unity's update by multi-threading? Will this cause any performance problem?
+        updateThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    while (!updateThread.isInterrupted()) {
+                        Thread.sleep(100);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // update TextView here!
+                                System.out.println("I am still running");
+                                UpdateUI();
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+       updateThread.start();
+
 
         // button for testing
+
+        //Monster
         Button button_attack = findViewById(R.id.button);
         Button button_reset = findViewById(R.id.button2);
         Button button_onFire = findViewById(R.id.button3);
@@ -48,7 +143,7 @@ public class MainActivity extends AppCompatActivity { //Leon: this script will a
             public void onClick(View view) {
                 monster.attack(10);
                 monster.endTurn();
-                UpdateUI();
+//                UpdateUI();
             }
         });
 
@@ -56,7 +151,7 @@ public class MainActivity extends AppCompatActivity { //Leon: this script will a
             @Override
             public void onClick(View view) {
                 monster.initialize();
-                UpdateUI();
+//                UpdateUI();
             }
         });
 
@@ -64,7 +159,10 @@ public class MainActivity extends AppCompatActivity { //Leon: this script will a
             @Override
             public void onClick(View view) {
                 monster.setOnFire();
-                UpdateUI();
+
+                //Leon: 借來用下 test 野
+                player.playerStartTurn();
+
             }
         });
 
@@ -72,14 +170,79 @@ public class MainActivity extends AppCompatActivity { //Leon: this script will a
             @Override
             public void onClick(View view) {
                 monster.setOffFire();
-                UpdateUI();
+
+                //Leon: 借來用下 test 野
+                player.playerEndTurn();
+
+
             }
         });
 
-        UpdateUI();
+
+        //Player
+        optionA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: call question evaluate answer function
+                System.out.println("Clicked OptionA");
+            }
+        });
+        optionB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: call question evaluate answer function
+                System.out.println("Clicked OptionB");
+            }
+        });
+        optionC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: call question evaluate answer function
+                System.out.println("Clicked OptionC");
+            }
+        });
+        optionD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: call question evaluate answer function
+                System.out.println("Clicked OptionD");
+            }
+        });
+        skill1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Skill1");
+            }
+        });
+        skill2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Skill2");
+                //TODO: call monster add all cool down function
+            }
+        });
+        skill3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                player.increaseAvailableTime();
+            }
+        });
+        skill4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Skill4");
+            }
+        });
+
+
+
+
+
+
     }
 
     public void UpdateUI() {
+        //Monster
         monsterHealthBar.setProgress(monster.getHealth());
         ability1_cd.setText("CD " + monster.getAbility_1_cd());
         ability2_cd.setText("CD " + monster.getAbility_2_cd());
@@ -93,5 +256,33 @@ public class MainActivity extends AppCompatActivity { //Leon: this script will a
             effect_fire1.setVisibility(View.INVISIBLE);
             effect_fire2.setVisibility(View.INVISIBLE);
         }
+
+        //Player
+        float alpha = 0f;
+        playerHealthBar.setProgress(player.getHealth());
+
+        skill1CD.setText(String.valueOf(player.skillsCoolDown[0]));
+        alpha = (player.skillsCoolDown[0] == 0) ? 0f: 0.8f;
+        skill1CD.setAlpha(alpha);
+        skill1CD.setClickable(player.skillsCoolDown[0]!=0);
+
+        skill2CD.setText(String.valueOf(player.skillsCoolDown[1]));
+        alpha = (player.skillsCoolDown[1] == 0) ? 0f: 0.8f;
+        skill2CD.setAlpha(alpha);
+        skill2CD.setClickable(player.skillsCoolDown[1]!=0);
+
+        skill3CD.setText(String.valueOf(player.skillsCoolDown[2]));
+        alpha = (player.skillsCoolDown[2] == 0) ? 0f: 0.8f;
+        skill3CD.setAlpha(alpha);
+        skill3CD.setClickable(player.skillsCoolDown[2]!=0);
+
+        skill4CD.setText(String.valueOf(player.skillsCoolDown[3]));
+        alpha = (player.skillsCoolDown[3] == 0) ? 0f: 0.8f;
+        skill4CD.setAlpha(alpha);
+        skill4CD.setClickable(player.skillsCoolDown[3]!=0);
+
+
+
+
     }
 }
--- app/src/main/java/hku/hkucs/mcrpg/Player.java
+++ app/src/main/java/hku/hkucs/mcrpg/Player.java
@@ -4,19 +4,31 @@ import android.os.CountDownTimer;
 
 public class Player {
 
-    private float health = 100f;
+    private int health = 100;
+    public int getHealth() {
+        return  health;
+    }
     private  float maximumAttackDamage = 50f;
     private CountDownTimer countDownTimer;
     private long timeLeft = 30000; //That is 30 sec
+    public long getTimeLeft() {
+        return timeLeft;
+    }
     private long timeAvailablePerQuestion = 30000; //That is 30 sec
+    private long additionTime = 0;
     private boolean heal;
-    private int removeOptions_CoolDown = 8;
-    private int delayEnemyAttack_CoolDown = 10;
-    private int heal_CoolDown = 4;
-    private int increaseAvailableTimeCoolDown = 6;
+    private int skill1CoolDown = 8; //eliminate wrong ans
+
+    private int skill2CoolDown = 10; //delay monster attack
+
+    private int skill3CoolDown = 6; //Increase thinking time
+
+    private int skill4CoolDown = 4; //heal
 
 
-    public void underAttack(float damage) {
+    public int[] skillsCoolDown = {skill1CoolDown, skill2CoolDown, skill3CoolDown, skill4CoolDown};
+
+    public void underAttack(int damage) {
             health -= damage;
             if (health < 0) {
                 //TODO: do sth to notify the player
@@ -37,33 +49,41 @@ public class Player {
     public void heal(float healAmount) {
         health+=healAmount;
         heal = false;
+        skillsCoolDown[3] = skill4CoolDown;
     }
 
     public void playerStartTurn() {
+
         resetTimer();
     }
 
     public void playerEndTurn() {
-        removeOptions_CoolDown--;
-        delayEnemyAttack_CoolDown--;
-        heal_CoolDown--;
-        increaseAvailableTimeCoolDown--;
 
-        stopTimer();
+        for (int i = 0; i < 4; i++) {
+            if (skillsCoolDown[i] > 0) {
+                skillsCoolDown[i]--;
+                System.out.println("cool down updated");
+            }
+        }
+//Add back timer later
+       stopTimer();
         heal = false; //To reset the effect if player use heal this turn;
+        additionTime = 0;
         timeAvailablePerQuestion = 30000; //To reset the effect if player use lengthen answer time this turn;
     }
 
     public void increaseAvailableTime() {
-        timeLeft+=30000; //we can directly increase the time left instead of restarting the timer
+        additionTime+=30000; //we can directly increase the additionTime instead of restarting the timer
         timeAvailablePerQuestion = 60000;  //we set it just to calculate the damage player dealt proportionally, this will be reset at the end of the turn
+        skillsCoolDown[2] = skill3CoolDown;
+        System.out.println("updated skill3 cd");
     }
 
     public void startTimer() {
         countDownTimer = new CountDownTimer(timeLeft,1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                timeLeft = millisUntilFinished;
+                timeLeft = additionTime +  millisUntilFinished;
                 //TODO: update the text in the timer with this timer UI;
             }
 
@@ -82,7 +102,8 @@ public class Player {
 
     public void resetTimer() {
         timeLeft = timeAvailablePerQuestion;
-        //TODO: update the timer text on the UI
+       startTimer();
+
     }
 
 
