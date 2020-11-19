b4c0f2850d4104a25e9c76805f10a7b3649eff69
rickychak <47154254+rickychak@users.noreply.github.com>
2020-11-19 11:44:09 +0800

    update qbank

--- app/src/main/java/hku/hkucs/mcrpg/questionBank.java
+++ app/src/main/java/hku/hkucs/mcrpg/questionBank.java
@@ -40,18 +40,37 @@ public class questionBank {
     questionStruct question30 = new questionStruct();
     questionStruct question31 = new questionStruct();
     questionStruct question32 = new questionStruct();
+    questionStruct question33 = new questionStruct();
+    questionStruct question34 = new questionStruct();
+    questionStruct question35 = new questionStruct();
+    questionStruct question36 = new questionStruct();
+    questionStruct question37 = new questionStruct();
+    questionStruct question38 = new questionStruct();
+    questionStruct question39 = new questionStruct();
+    questionStruct question40 = new questionStruct();
+    questionStruct question41 = new questionStruct();
+    questionStruct question42 = new questionStruct();
+    questionStruct question43 = new questionStruct();
+    questionStruct question44 = new questionStruct();
+    questionStruct question45 = new questionStruct();
+    questionStruct question46 = new questionStruct();
+    questionStruct question47 = new questionStruct();
+    questionStruct question48 = new questionStruct();
+    questionStruct question49 = new questionStruct();
+
+
 
 
 
     //question bank, hardcoded
     public void infoBase() {
-        question1.setter("Chemistry", 1, "What is the atomic number of Aluminium", "13", "14", "15", "16", 0);
+        question1.setter("Chemistry", 1, "What is the atomic number of Aluminium?", "13", "14", "15", "16", 0);
         question2.setter("Mathematics", 4, "What is the answer of 1+2+3+...+48+49+50?", "1000", "1175", "1200", "1275", 3);
         question3.setter("English", 1, "Where does the world \"Cliche\" come from?", "Spain", "France", "Egypt", "America", 1);
         question4.setter("Geography", 3, "What is the name of the second highest mountain?", "Mount Everest", "K2", "Cho Oyu", "Lhotse", 1);
         question5.setter("Biology", 2, "What is the range length of male blue whale penis?", "1 to 2 meters", "2 to 3 meters", "3 to 4 meters", "30cm", 1);
         question6.setter("Tricky Question", 1, "What is always coming, but never arrives?", "Winter", "Hi-bye friend", "Tomorrow", "The tortoise from Zeno's paradox", 2);
-        question7.setter("Philosophy", 4, "Who is the authur of Discours de la methode？", "Friedrich Nietzsche", "Aristotle", "Socrates", "René Descartes", 3);
+        question7.setter("Philosophy", 4, "Who is the authur of \"Discours de la methode\" ? ", "Friedrich Nietzsche", "Aristotle", "Socrates", "René Descartes", 3);
         question8.setter("Computer Science", 4, "What is the time complexity of breadth-first search of a graph?", "O(V)", "O(E)", "O(V+E)", "O(V-E)", 2);
         question9.setter("Geography", 2, "What country does the city \"Fucking\" locate at?", "Bulgaria", "Australia", "Austria", "Argentina", 2);
         question10.setter("English", 2, "What work is not written by William Shakespeare?", "Dracula", "Othello", "Harmlet", "Macbeth", 0);
@@ -61,12 +80,12 @@ public class questionBank {
         question14.setter("Geography", 3, "What is the name of the smallest country in the world?", "Monaco", "Nauru", "Tuvalu","Vatican City",3);
         question15.setter("Biology", 3, "How much water does a human have in their brain approximately?", "90%" , "50%" , "60%" , "70%" , 3);  
         question16.setter("Animal Studies", 1, "What is the heart beat rate of a sloth?" ,"150bpm", "10bpm", "80bpm" , "120bpm", 2);    
-        question17.setter("Chemistry", 5, "Who invented the first full functional mass spectrometer?", "Robert Bunson", "Michael Faraday", "Francis William Aston", "Ernest Rutherford" , 2);  
-        question18.setter("Physics", 5, "At what speed does the Earth rotate itself at Earth's equator?", "2000 miles/h", "3000 miles/h" , "1000 miles/h", "4000 miles/h", 2);
+        question17.setter("Chemistry", 4, "Who invented the first full functional mass spectrometer?", "Robert Bunson", "Michael Faraday", "Francis William Aston", "Ernest Rutherford" , 2);
+        question18.setter("Physics", 4, "At what speed does the Earth rotate itself at Earth's equator?", "2000 miles/h", "3000 miles/h" , "1000 miles/h", "4000 miles/h", 2);
         question19.setter("Film Studies", 1, "Who is the director of the famous movie \"Inception\"?", "James Cameron", "Christopher Nolan", "The Wachowskis", "Peter Jackson", 1);
         question20.setter("History", 2, "When did the World War II end?", "1940", "1950" , "1935" ,"1945",3);
         question21.setter("Music", 1, "Who did Ed Sheeran collaborate with in the song \"Beautiful People\"?", "Justin Bieber", "Khalid", "Cardi B", "Bruno Mars", 1);    
-        question22.setter("Mathematics", 2, "What is the correct answer of pi in 6 deciml places?", "3.14159266", "3.14159265", "3.14159263", "3.14159264", 1);
+        question22.setter("Mathematics", 2, "What is the correct answer of pi in 8 decimal places?", "3.14159266", "3.14159265", "3.14159263", "3.14159264", 1);
         question23.setter("English", 1, "What is the meaning of the word \"hangry\"?", "handing angrily" , "handling angrily" , "hungry angrily" ,"hanging angrily", 2);
         question24.setter("French", 1, "What does the word \"Oui\" mean in French?", "Yes", "No", "He", "She", 0);
         question25.setter("Geography", 2, "What is the name of the shortest river in the world?" , "Roe River", "D River", "Amur" , "Amon", 0);
@@ -74,11 +93,26 @@ public class questionBank {
         question27.setter("Gaming", 3, "When did the famous game \"League of Legends\" be released?", "2010" , "2012" ,"2009", "2013", 2);
         question28.setter("Youtuber", 1, "What is the name of the most subscribed youtube channel in 2018?", "T-Series", "MrBeast" , "Vanossgaming", "Pewdiepie", 3);
         question29.setter("Technology", 3, "What autonomous driving level has Tesla achieved in 2019?", "1", "2", "3" ,"4", 1 );
-        question30.setter("Botany", 1 , "How much water is in Apple(fruit)?" , "50%" , "25%" , "75%" , "10%", 1);
+        question30.setter("Botany", 1 , "How much water is in an apple?" , "50%" , "25%" , "75%" , "10%", 1);
         question31.setter("Korean", 5, "저는 김민수입니다. 이 사람은 제임스입니다", "시간", "장소", "이름", "주말", 2);
         question32.setter("Korean", 5, "머리가 깁니다. 그래서 (     )싶습니다.", "자르고", "나오고", "가지고", "마시고", 0);
-
-
+        question33.setter("Philosophy", 1, "Below are the names of famous philosophers. Which one is spelled correctly?","Witgenstein" , "Roussaeu", "Nietzche", "Kierkegaard", 3);
+        question34.setter("Philosophy",3, "What university ranked first in QS 2020 ranking in philosophy?", "CUHK",  "HKU", "HKBU" ,"HSU", 0);
+        question35.setter("Physics",4, "What is the coldest planet in the solar system?", "Mars", "Jupiter", "Uranus", "Mercury", 2 );
+        question36.setter("Physics",1, "What is the name of the process of solid transforming into gas directly?", "Evaporation", "Sublimation", "Condensation", "Gassification", 1);
+        question37.setter("Music",4, "What era does Chopin belong to?", "Romantic", "Classical", "Baroque", "Renaissance", 0 );
+        question38.setter("Cooking",1, "What material must not be added while whisking the egg white into whip egg white?", "Lemon Juice", "Sugar", "Oil", "Water", 2 );
+        question39.setter("Chemistry",1, "What key chemical is in baking powder to make the food raise?", "Sodium chloride", "Sodium dichromate", "Sodium fluoride", "Sodium carbonate", 3 );
+        question40.setter("Mathematics",1, "Choose the correct option: 5 ( ) 5 ( ) 5 = 6", "÷ +", "x +", "+ -", "x -", 0 );
+        question41.setter("English",2, "What does \"Salty\" mean nowadays?", "Upset", "Happy", "Worried", "Drunk", 0 );
+        question42.setter("Portuguese",2,  "What does \"amor\" mean in Portuguese?","Hello", "Bye" , "Babe", "Honey", 2);
+        question43.setter("Animal Studies",2, "How many teeths do a camel have?", "40" , "38" , "36", "34", 3 );
+        question44.setter("Gender Studies",2, "What does the \"2S\" mean in LGBTQQIP2SAA?", "twosexual", "to-sexual", "two-spirit", "two-sense", 2);
+        question45.setter("Medical",1, "Which of the following state is legal to consume marijuana for recreational usage?", "New York", "California", "Texas", "Wyoming",1);
+        question46.setter("Thai", 5, "แกนีมีดเป็นดาวบริวารที่มีขนาดใหญ่ที่สุดของ", "ดาวพฤหัสบดี", "ดาวเสาร์ ", "คดาวยูเรนัส", "ดาวเนปจูน", 0);
+        question47.setter("Japanese", 5, "ここは（　　）です。べんきょうできません" , "くらい", "さむい" ,"うるさい" ,"あぶない", 2);
+        question48.setter("Japanese", 5, "おとうと　へや（　　）そうじを　しました", "が" , "を" , "に" , "の", 3);
+        question49.setter("French", 5, "Est-ce que tu es informe des (     ) nouvelles?",  "sont", "dernieres" , "regarder", "d'avancer", 1);
 
         //for further improvement, can build another arraylist of problems for hardness level 5 instead of mixing it in general questionSet
         questionBank.add(question1);
@@ -111,9 +145,28 @@ public class questionBank {
         questionBank.add(question28);
         questionBank.add(question29);
         questionBank.add(question30);
+        questionBank.add(question33);
+        questionBank.add(question34);
+        questionBank.add(question35);
+        questionBank.add(question36);
+        questionBank.add(question37);
+        questionBank.add(question38);
+        questionBank.add(question39);
+        questionBank.add(question40);
+        questionBank.add(question41);
+        questionBank.add(question42);
+        questionBank.add(question43);
+        questionBank.add(question44);
+        questionBank.add(question45);
+
 
         hardQuestionBank.add(question31);
         hardQuestionBank.add(question32);
+        hardQuestionBank.add(question46);
+        hardQuestionBank.add(question47);
+        hardQuestionBank.add(question48);
+        hardQuestionBank.add(question49);
+
 
         System.out.println("Q bank size in info base:" + questionBank.size());
     }
@@ -123,6 +176,7 @@ public class questionBank {
     //shuffling the questionSet
     public void shuffling() {
         Collections.shuffle(questionBank);
+        Collections.shuffle(hardQuestionBank);
 
         //For testing
         System.out.println("Q Bank size = "+questionBank.size());
@@ -130,7 +184,7 @@ public class questionBank {
             System.out.println(questionBank.get(i).question);
         }
 
-        Collections.shuffle(hardQuestionBank);
+
     }
     //checking if the answer is correct
     public boolean evaluation(int option, questionStruct QuestionInFunction) {
